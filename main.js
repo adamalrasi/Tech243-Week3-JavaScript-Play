8c08cb561610256f27bf0fdbd1ea0535150b0625
Adam Alrasi <adam.alrasi1@gmail.com>
2024-03-05 17:21:44 +0000

    feat: adding to the javascript playground

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
-window.alert("Hello World");;
+// window.alert("Hello World");
 document.write("oh, and if i don't see ya");
+const actor = {};
+actor.firstName = "Jim";
+actor.lastName = "Carrey";
+actor.getFullName = function() { return this.firstName + " " + this.lastName };
+
+console.log(actor.getFullName());
